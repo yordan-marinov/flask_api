6baf30217de462ba737136850683c9fe22c966f7
yordan-marinov <jordanmarinov8@gmail.com>
2021-11-30 17:21:03 +0200

    Refactoring code

--- client_request.py
+++ client_request.py
@@ -1,24 +1,16 @@
 import requests
 import pprint
 
-# Base url "Home"
 HOME_URL = "http://localhost:5000"
-# All the currency rates
-API_CONVERT = "/api/calculate"
-GET_CURRENCY = "/api/"
+GET_ALL_CURRENCIES = "/api/all"
+API_CONVERT = "/api/convert"
 
-# r = requests.get(f"{HOME_URL}{API_CONVERT}/usd")
-payload = {'currency_one': 'USD', 'currency_two': 'BGN', 'amount': 100}
-# req = requests.get(f"{HOME_URL}{API_CONVERT}", params=payload)
-req = requests.get(f"{HOME_URL}{GET_CURRENCY}", params=payload)
+payload = {'base_currency': 'USD', 'to_currency': 'BGN', 'amount': 100}
+response = requests.get(f"{HOME_URL}{API_CONVERT}", params=payload)
 
 try:
-    # r.raise_for_status()
-    req.raise_for_status()
+    response.raise_for_status()
 except Exception as error:
     print(error)
 
-
-
-pprint.pprint(req)
-
+pprint.pprint(response.json())
